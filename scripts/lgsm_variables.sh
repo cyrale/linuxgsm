31317e4ab3c50cc07e30184f50c9fab387c95895
Cyril Jacquesson <cyril@jacquesson.me>
2021-01-21 19:47:20 +0100

    prevent redefinition of variables

--- scripts/lgsm_variables.sh
+++ scripts/lgsm_variables.sh
@@ -4,12 +4,17 @@ source /lgsm_functions.sh
 
 fn_check_user
 
-lgsm_script="${HOME}/linuxgsm.sh"
-lgsm_dir="${HOME}/lgsm"
-config_dir="${lgsm_dir}/config-lgsm"
-server_config_dir="${config_dir}/${LGSM_GAMESERVER}"
-server_config_file="${server_config_dir}/${LGSM_GAMESERVER}.cfg"
-common_config_file="${server_config_dir}/common.cfg"
+if [ -z "${lgsm_variables_defined}" ]
+then
+    lgsm_script="${HOME}/linuxgsm.sh"
+    lgsm_dir="${HOME}/lgsm"
+    config_dir="${lgsm_dir}/config-lgsm"
+    server_config_dir="${config_dir}/${LGSM_GAMESERVER}"
+    server_config_file="${server_config_dir}/${LGSM_GAMESERVER}.cfg"
+    common_config_file="${server_config_dir}/common.cfg"
 
-gameserver_original_script="${HOME}/${LGSM_GAMESERVER}"
-gameserver_renamed_script="${HOME}/$(fn_sanitize_string ${LGSM_GAMESERVER_RENAME})"
+    gameserver_original_script="${HOME}/${LGSM_GAMESERVER}"
+    gameserver_renamed_script="${HOME}/$(fn_sanitize_string ${LGSM_GAMESERVER_RENAME})"
+
+    lgsm_variables_defined="true"
+fi
